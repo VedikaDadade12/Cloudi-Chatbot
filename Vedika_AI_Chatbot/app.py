f56487e894ff1ece28f277a45f1aa796b84af159
VedikaDadade12 <vedikadabade8@gmail.com>
2025-08-04 17:33:16 +0530

    app.py

--- Vedika_AI_Chatbot/app.py
+++ Vedika_AI_Chatbot/app.py
@@ -428,5 +428,9 @@ def send_sms(to, message):
 
 # ----------- Run App -----------
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    from dotenv import load_dotenv
+    load_dotenv()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+
